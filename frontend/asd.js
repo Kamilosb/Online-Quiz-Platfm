fe7290698b6c67da1f9a28eb21e1045cf961b403
Kamilosb <Kamilosb@duck.com>
2023-10-04 10:20:16 +0200

    test implementation of adding html elements using js

--- frontend/asd.js
+++ frontend/asd.js
@@ -1,4 +1,14 @@
-axios.get('api/quiz/all')
-        .then((response) => {
-            console.log(response)
-        })
+// axios.get('api/quiz/all')
+//         .then((response) => {
+//             console.log(response)
+//         })
+
+let pop = document.getElementsByClassName('popularne-1')
+
+function addNewBox(title) {
+    const newBox = `<div class="rectangle">\n<div class="rectangle-2"></div>\n<div class="text-wrapper-5">${title}</div>`
+    console.log(pop[0].innerHTML)
+    pop[0].innerHTML += `${newBox}`
+}
+
+addNewBox('UIASDGASd')
