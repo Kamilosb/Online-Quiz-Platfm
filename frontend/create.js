d04a9ef5b0988205c260b4962a2f531376baf199
Frubas <dawid.brychcy21345@gmail.com>
2023-11-21 14:14:27 +0100

    gkjhkkhj

--- frontend/create.js
+++ frontend/create.js
@@ -1,23 +1,29 @@
-        // const axios = require('axios')
-        axios.get('/quiz/all')
-        .then((response) => {
-            console.log(response)
-        })
+// const axios = require('axios')
+axios.get('/quiz/all')
+    .then((response) => {
+        console.log(response)
+    })
 
-        function sendQuiz() {
-            const name = document.getElementById('name')
-            const desc = document.getElementById('desc')    
-            const reqBody = {
-                "name": name.value,
-                "description": desc.value
-            }
-            axios.post('/quiz/', reqBody)
-                .then(function (response) {
-                    // handle success
-                    console.log(response);
-                })
-                .catch(function (error) {
-                    // handle error
-                    console.log(error);
-                })
-        }    
+function sendQuiz() {
+    const name = document.getElementById('name')
+    const desc = document.getElementById('desc')
+    const reqBody = {
+        "name": name.value,
+        "description": desc.value
+    }
+    axios.post('/quiz/', reqBody)
+        .then(function (response) {
+            // handle success
+            console.log(response);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+}
+function addQuizQuestion() {
+    const quizElement = '<div class="odpowiedzi">' + document.getElementsByClassName('odpowiedzi')[0].innerHTML + '</div>'
+    const odp = document.getElementById('form')
+    odp.innerHTML += quizElement
+    console.log(odp)
+}
